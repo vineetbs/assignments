d5735129e046e7242f75f1881091aab1bbc376ca
vineetbs <thevineetbimal@gmail.com>
2024-09-18 22:30:15 +0530

    chore: Update express dependency to version 4.19.2 and fix formatting in todoServer.js and fileServer.js

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -7,15 +7,47 @@
     Example: GET http://localhost:3000/files
   2. GET /file/:filename - Returns content of given file by name
      Description: Use the filename from the request path parameter to read the file from `./files/` directory
-     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
+     Response: 200 OK with the file content as the response body if found, 
+     or 404 Not Found if not found. Should return `File not found` as text if file is not found
      Example: GET http://localhost:3000/file/example.txt
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const app = express();
 
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const filesDir = path.join(__dirname, "./files");
+
+app.get("/files", (req, res) => {
+  fs.readdir(filesDir, (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+
+    res.status(200).json(files);
+  });
+});
+
+app.get("/file/:name", (req, res) => {
+  const filename = path.join(__dirname, "./files/", req.params.name);
+
+  fs.readFile(filename, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(404).send("File not found");
+    }
+    res.send(data);
+  });
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("Route not found");
+});
 
 module.exports = app;
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
